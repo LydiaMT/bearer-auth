d3e49a27f321d1eb72ccb99ec9755ee858d0d585
Lydia Minehan-Tubic <lydiaminehantubic@gmail.com>
2021-04-27 19:53:46 -0700

    deploy

--- src/auth/models/users.js
+++ src/auth/models/users.js
@@ -8,7 +8,6 @@ process.env.SECRET = 'toes'
 const users = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  // ADDED
 });
 
 // Adds a virtual field to the schema. We can see it, but it never persists
